428a4fb26d24b8134259629fc1f7821798ed88da
TranKhanhAn68 <2351050004an@ou.edu.vn>
2026-05-05 23:23:05 +0700

    Update

--- backend/run_django.sh
+++ backend/run_django.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 echo "🚀 Activate virtual environment..."
-source venv/bin/activate
+source .venv/bin/activate   # nếu bạn dùng .venv
 
-echo "📦 Apply migrations..."
-python manage.py makemigrations
-python manage.py migrate
+echo "🧹 Flush database..."
+py manage.py flush --no-input
 
-echo "📥 Load initial data..."
-python manage.py loaddata authors.json
+echo "📦 Migrate..."
+py manage.py migrate
+
+echo "=== Tạo superuser ==="
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@example.com
+export DJANGO_SUPERUSER_PASSWORD=Admin@123
+py manage.py createsuperuser --no-input || echo "Superuser đã tồn tại!"
+
+echo "📥 Load data..."
+py manage.py loaddata db.json/Author.json
+py manage.py loaddata db.json/Category.json
+py manage.py loaddata db.json/Publisher.json
+py manage.py loaddata db.json/Book.json
+py manage.py loaddata db.json/Setting.json
+
+
+
+python manage.py createsuperuser --no-input || echo "Superuser đã tồn tại!"
 
 echo "🖥️ Run server..."
-python manage.py runserver
+py manage.py runserver

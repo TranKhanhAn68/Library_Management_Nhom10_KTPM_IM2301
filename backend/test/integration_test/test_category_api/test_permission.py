6d580fcf7963ebbc8121cd032133b8fe8d797d12
nganphung18 <pkngeun12@gmail.com>
2026-05-14 15:32:53 +0700

    update

--- backend/test/integration_test/test_category_api/test_permission.py
+++ backend/test/integration_test/test_category_api/test_permission.py
@@ -1,6 +1,8 @@
 import pytest
-from library_management.models import *
+
+from library_management.models import Category
 from test.integration_test.helpers import generate_permissions_matrix
+
 
 @pytest.mark.django_db
 class TestCategoryPermissions:
@@ -9,11 +11,42 @@ class TestCategoryPermissions:
         "method, url, user_fixture, expected",
         generate_permissions_matrix("/categories/")
     )
-    def test_all_permissions_category(self, request, method, url, user_fixture, expected):
+    def test_all_permissions_category(
+        self,
+        request,
+        method,
+        url,
+        user_fixture,
+        expected
+    ):
+
         client = request.getfixturevalue(user_fixture)
 
-        data_form = {"name": "Refactoring"} if method in ["post", "put", "patch"] else {}
+        # tạo category test
+        category = Category.objects.create(
+            name="Test Category"
+        )
 
-        response = getattr(client, method)(url, data=data_form)
-    
-        assert response.status_code == expected
+        # replace URL detail
+        if "/1/" in url:
+            url = url.replace(
+                "/1/",
+                f"/{category.id}/"
+            )
+
+        data_form = {
+            "name": "Refactoring"
+        } if method in ["post", "put", "patch"] else {}
+
+        response = getattr(
+            client,
+            method
+        )(
+            url,
+            data=data_form
+        )
+
+        if method == "delete" and user_fixture == "auth_admin":
+            assert response.status_code == 204
+        else:
+            assert response.status_code == expected
